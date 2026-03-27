aa611ad57c24a6d4c40b90a958f1b4254b2ad99c
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-03-28 00:08:47 +0800

    fix: move Card test code from Homepage to Catalog page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-    import Card from "$lib/components/Card/Card.svelte"; // for Card testing
+
 </script>
 
 <h1>This is the HomePage</h1>
 <p>TODO</p>
-
-<!-- For Card testing -->
-<div class="flex gap-10">
-{#each [1, 2, 3, 4] as _}
-    <Card image="./csi_logo.png" price={19.99} name="CSI Merchandise" />
-{/each}
-</div>
--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+    import Card from "$lib/components/Card/Card.svelte"; // for Card testing
+</script>
+
 <h1>This is the Catalog Page</h1>
+
+<!-- For Card testing -->
+<div class="flex gap-10">
+{#each [1, 2, 3, 4] as _}
+    <Card image="./csi_logo.png" price={19.99} name="CSI Merchandise" />
+{/each}
+</div>
